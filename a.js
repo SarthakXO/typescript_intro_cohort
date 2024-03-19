7cad4fdb1659c4920c5143a3b76a2c112c22f74a
Sarthak <sarthakbhatt@live.com>
2024-03-19 22:05:51 +0530

    solved problem 2

--- a.js
+++ a.js
@@ -1,6 +1,13 @@
 "use strict";
 let x = 1;
+//problem 1 give types to arguments
 const greet = (firstName) => {
-  console.log("Hello " + firstName);
+    console.log("Hello " + firstName);
 };
-greet("Sarthak");
+console.log(greet("Sarthak"));
+//problem 2 give type to the returned value
+const sum = (a, b) => {
+    return a + b;
+};
+const value = sum(1, 2);
+console.log(value);
--- a.ts
+++ a.ts
@@ -1,7 +1,16 @@
 let x: number = 1;
 
+//problem 1 give types to arguments
 const greet = (firstName: string) => {
   console.log("Hello " + firstName);
 };
 
 console.log(greet("Sarthak"));
+
+//problem 2 give type to the returned value
+const sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+const value = sum(1, 2);
+console.log(value);
