7656522ecd81f029f10723473022ac9eff957de3
Sarthak <sarthakbhatt@live.com>
2024-03-28 03:19:23 +0530

    completed assignment

--- src/enums.ts
+++ src/enums.ts
@@ -1,3 +1,16 @@
-const doSomething = (keyPressed) => {
+type KeyInput = "up" | "down" | "left" | "right"; //workable way
+
+enum Direction {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+const doSomething = (keyPressed: Direction) => {
   //do something
 };
+
+doSomething(Direction.Up);
+doSomething(Direction.Down);
+// doSomething("downrasdasd");
