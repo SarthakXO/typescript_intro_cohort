a40d568357ff4a2522f1fb8a2eecdaa6b9283b27
Sarthak <sarthakbhatt@live.com>
2024-03-29 00:41:13 +0530

    solved the orignal problem

--- dist/generics.js
+++ dist/generics.js
@@ -1,6 +1,10 @@
 "use strict";
+function identity(arg) {
+    return arg;
+}
+let ouput1 = identity("MyString");
+let ouput2 = identity(1234);
 const firstEl = (arr) => {
     return arr[0];
 };
-const firstElValue = firstEl(["sarthak", "bhatt"]);
-console.log(firstElValue);
+console.log(firstEl(["sarthak", "bhatt"]).toUpperCase());
--- src/generics.ts
+++ src/generics.ts
@@ -4,3 +4,10 @@ function identity<T>(arg: T) {
 
 let ouput1 = identity<string>("MyString");
 let ouput2 = identity<number>(1234);
+
+//orignal problem
+const firstEl = <T>(arr: T[]) => {
+  return arr[0];
+};
+
+console.log(firstEl(["sarthak", "bhatt"]).toUpperCase());
